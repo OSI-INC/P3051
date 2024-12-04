d306cf52f76712dfb3840144de8ae41cb3c912b7
Kevan Hashemi <hashemi@opensourceinstruments.com>
2024-12-04 18:39:03 -0500

    Add eight-bit I2C write, but disable. Reading ID byte.

--- ROM.asm
+++ ROM.asm
@@ -5,7 +5,7 @@
 
 ; Calibration Constants
 const tx_frequency      5  ; Transmit frequency calibration
-const device_id        65  ; Will be used as the first channel number.
+const device_id        66  ; Will be used as the first channel number.
 const sample_period     0  ; Sample period in units of RCK periods, use 0 for 256.
 
 ; Address Map Boundary Constants
@@ -71,10 +71,388 @@ jp interrupt
 
 
 ; ------------------------------------------------------------
+; The interrupt routine. Reads the sensor and transmits the
+; measurement with the sample transmitter.
+
+interrupt:
+
+; Save A on the stack, set bit zero to one, use to enable
+; the transmit clock and then boost the CPU to TCK. Push
+; F, B, and C onto stack.
+push A       
+ld A,0x01          
+ld (mmu_etc),A    
+ld (mmu_bcc),A
+push F  
+push B  
+push C
+
+; Set the zero bit of the diagnostic flag register. We can
+; direct this bit to a test pin to see if the interrupt is
+; happening. We read the register and OR with 0x01 to set
+; the zero bit.
+ld A,(mmu_dfr)     
+or A,0x01          
+ld (mmu_dfr),A  
+
+; Sixteen-bit read from sensor. The first byte will be in B
+; and the second in A.
+ld A,ps_WHO_AM_I
+call i2c_rd16
+
+; Transfer the two bytes into the transmit data registers.
+ld (mmu_xlb),A
+push B
+pop A
+ld (mmu_xhb),A
+
+; Write the device identifier to the transmit channel number register.
+ld A,device_id   
+ld (mmu_xcn),A 
+
+; Initiate transmission with any write to the transmission control
+; register.
+ld (mmu_xcr),A 
+
+; Wait for transmission to complete. We must keep the transmit clock going
+; so the state machine will finish its transmission.
+ld A,tx_delay    
+dly A            
+
+; Reset all interrupts.
+ld A,0xFF   
+ld (mmu_irst),A   
+
+; Clear bit zero of the diagnostic flag register to indicate the end
+; of the interrupt.
+ld A,(mmu_dfr)  
+and A,0xFE   
+ld (mmu_dfr),A 
+
+; Restore C, B, and F.
+pop C
+pop B
+pop F 
+
+; Move the CPU out of boost, stop the transmit clock, pop A, and 
+; return from interrupt.
+ld A,0x00        
+ld (mmu_bcc),A    
+ld (mmu_etc),A 
+pop A
+rti  
+
+; ------------------------------------------------------------
+; Calibrate the transmit clock frequency. Will leave the
+; transmit clock disabled and cpu boost turned off. The 
+; transmit clock should run at 5 MHz. We are going to determine
+; which value of transmit clock divisor, when applied to the
+; ring oscillator frequency, gives us a frequency close to
+; 5 MHz. We will write that value to the transmit clock divisor
+; register, and so calibrate the ring oscillator.
+
+calibrate_tck:
+
+; Push the registers we are going to use.
+push F
+push A           
+push B   
+
+; Disable the CPU boost clock, to enter non-boost, where we are
+; running off 32.768 kHz.
+ld A,0x00    
+ld (mmu_bcc),A   
+
+; Turn off the transmit clock (TCK) by writing a zero to the
+; enable transmit clock location in our control space. 
+ld (mmu_etc),A
+
+; Pick an initial value for the divisor that we know will be 
+; too high, so the transmit clock frequency will certainly
+; be too low. Store this value in both A and B registers. There
+; is no instruction to move A directly into B. Instead, we push
+; A onto the stack, then pop it off into B.
+ld A,max_tcd 
+push A  
+pop B
+
+; We enter a loop in which we decrement the divisor stored in
+; B, then try it out and see if the resulting frequency is 
+; above the minimum frequency we are prepared to accept. We
+; measure the frequency using the transmit clock frequency
+; register, which counts the number of TCK cycles in one
+; half of an RCK cycle immediately after we enable the 
+; transmit clock. When we have this number of cycles, we 
+; subtract a minimum, and if the result is negative, we know
+; we have to decrease the divisor further. If the result is
+; positive, we are done.
+cal_tck_1:
+dec B           
+push B           
+pop A            
+ld (mmu_tcd),A   
+ld A,0x01        
+ld (mmu_etc),A   
+ld A,(mmu_tcf)  
+sub A,min_tcf   
+ld A,0x00       
+ld (mmu_etc),A   
+jp np,cal_tck_1 
+
+; Pop and return.
+pop B            
+pop A          
+pop F
+ret              
+
+
+; ------------------------------------------------------------
+; Initialize the device. We will be setting up the stack, calibrating
+; the ring oscillator, and configuring the sensor.
+
+initialize:
+
+; Initialize the stack pointer.
+ld HL,mmu_sba
+ld SP,HL
+
+; The boot delay allows the power supplies to settle before we calibrate
+; the ring oscillator.
+ld A,boot_delay  ; We want boot_delay
+push A           ; multiplied by 256 cycles of RCK
+pop B            ; before proceeding.
+pwr_up_lp:
+ld A,0xFF        ; Load A with 255
+dly A            ; and delay,
+dec B            ; then decrement B.
+jp nz,pwr_up_lp
+
+; Calibrate the transmit clock.
+call calibrate_tck
+
+; Set the low radio frequency for sample transmission
+ld A,tx_frequency
+ld (mmu_xfc),A
+
+; Set interrupt timer interval and enable the timer interrupt to implement
+; the sample period. The value we want to load into the interrup timer period 
+; register is the sample period minus one, because the interrupt timer counts
+; the value down to zero. So we load A with sample_period, then decrement. If
+; sample_period is zero (0x00), the value we write is 255 (0xFF).
+ld A,0xFF           
+ld (mmu_irst),A  
+ld A,sample_period 
+dec A 
+ld (mmu_itp),A 
+ld A,0x01 
+ld (mmu_imsk),A 
+
+; Done with initialization, jump to main loop.
+jp main
+
+
+; ------------------------------------------------------------
+
+; The main program loop. The interrupts will be running 
+; in the background, and they do all the work. The main
+; routine generates a pulse on bit one of the diagnostic
+; flag register, and it implements a delay so these 
+; pulses are rare.
+
+main:
+
+ld A,(mmu_dfr) 
+or A,0x02
+ld (mmu_dfr),A
+and A,0xFD
+ld (mmu_dfr),A
+ld A,255  
+dly A      
+jp main
+
+
+; ------------------------------------------------------------
+; I2C Eight-Bit Write. Write to one register location on the
+; sensor. The address of the register should be passed into
+; the routine in the accumulator and the byte to be written should
+; be passed in C.
+
+i2c_wr8:
+
+; Store the sub-address address in B.
+
+push A            ; 1
+pop B             ; 2
+       
+; I2C: Start code (ST)
+
+ld (mmu_i2cZ1),A  ; 3
+ld (mmu_i2c01),A  ; 3
+ld (mmu_i2c00),A  ; 3
+
+; I2C: Write seven-bit device address and !WRITE flag (SAD+W).
+
+ld A,ps_SAD       ; 2
+sla A             ; 1
+ld (mmu_i2cA0),A  ; 3
+ld (mmu_i2cA1),A  ; 3
+ld (mmu_i2cA0),A  ; 3
+
+rl A              ; 1
+ld (mmu_i2cA0),A  ; 3
+ld (mmu_i2cA1),A  ; 3
+ld (mmu_i2cA0),A  ; 3
+
+rl A              ; 1
+ld (mmu_i2cA0),A  ; 3
+ld (mmu_i2cA1),A  ; 3
+ld (mmu_i2cA0),A  ; 3
+
+rl A              ; 1
+ld (mmu_i2cA0),A  ; 3
+ld (mmu_i2cA1),A  ; 3
+ld (mmu_i2cA0),A  ; 3
+
+rl A              ; 1
+ld (mmu_i2cA0),A  ; 3
+ld (mmu_i2cA1),A  ; 3
+ld (mmu_i2cA0),A  ; 3
+
+rl A              ; 1
+ld (mmu_i2cA0),A  ; 3
+ld (mmu_i2cA1),A  ; 3
+ld (mmu_i2cA0),A  ; 3
+
+rl A              ; 1
+ld (mmu_i2cA0),A  ; 3
+ld (mmu_i2cA1),A  ; 3
+ld (mmu_i2cA0),A  ; 3
+
+rl A              ; 1
+ld (mmu_i2cA0),A  ; 3
+ld (mmu_i2cA1),A  ; 3
+ld (mmu_i2cA0),A  ; 3
+
+; I2C: Accept slave acknowledgement (SAK).
+
+ld (mmu_i2cZ0),A  ; 3
+ld (mmu_i2cZ1),A  ; 3
+ld (mmu_i2cZ0),A  ; 3
+
+; I2C: Write eight-bit sub address (SUB), which is
+; stored in B.
+
+push B            ; 1
+pop A             ; 2
+ld (mmu_i2cA0),A  ; 3
+ld (mmu_i2cA1),A  ; 3
+ld (mmu_i2cA0),A  ; 3
+
+rl A              ; 1
+ld (mmu_i2cA0),A  ; 3
+ld (mmu_i2cA1),A  ; 3
+ld (mmu_i2cA0),A  ; 3
+
+rl A              ; 1
+ld (mmu_i2cA0),A  ; 3
+ld (mmu_i2cA1),A  ; 3
+ld (mmu_i2cA0),A  ; 3
+
+rl A              ; 1
+ld (mmu_i2cA0),A  ; 3
+ld (mmu_i2cA1),A  ; 3
+ld (mmu_i2cA0),A  ; 3
+
+rl A              ; 1
+ld (mmu_i2cA0),A  ; 3
+ld (mmu_i2cA1),A  ; 3
+ld (mmu_i2cA0),A  ; 3
+
+rl A              ; 1
+ld (mmu_i2cA0),A  ; 3
+ld (mmu_i2cA1),A  ; 3
+ld (mmu_i2cA0),A  ; 3
+
+rl A              ; 1
+ld (mmu_i2cA0),A  ; 3
+ld (mmu_i2cA1),A  ; 3
+ld (mmu_i2cA0),A  ; 3
+
+rl A              ; 1
+ld (mmu_i2cA0),A  ; 3
+ld (mmu_i2cA1),A  ; 3
+ld (mmu_i2cA0),A  ; 3
+
+; I2C: Accept slave acknowledgement (SAK).
+
+ld (mmu_i2cZ0),A  ; 3
+ld (mmu_i2cZ1),A  ; 3
+ld (mmu_i2cZ0),A  ; 3
+
+; I2C: Write the eight data bits.
+
+push C            ; 1
+pop A             ; 2
+ld (mmu_i2cA0),A  ; 3
+ld (mmu_i2cA1),A  ; 3
+ld (mmu_i2cA0),A  ; 3
+
+rl A              ; 1
+ld (mmu_i2cA0),A  ; 3
+ld (mmu_i2cA1),A  ; 3
+ld (mmu_i2cA0),A  ; 3
+
+rl A              ; 1
+ld (mmu_i2cA0),A  ; 3
+ld (mmu_i2cA1),A  ; 3
+ld (mmu_i2cA0),A  ; 3
+
+rl A              ; 1
+ld (mmu_i2cA0),A  ; 3
+ld (mmu_i2cA1),A  ; 3
+ld (mmu_i2cA0),A  ; 3
+
+rl A              ; 1
+ld (mmu_i2cA0),A  ; 3
+ld (mmu_i2cA1),A  ; 3
+ld (mmu_i2cA0),A  ; 3
+
+rl A              ; 1
+ld (mmu_i2cA0),A  ; 3
+ld (mmu_i2cA1),A  ; 3
+ld (mmu_i2cA0),A  ; 3
+
+rl A              ; 1
+ld (mmu_i2cA0),A  ; 3
+ld (mmu_i2cA1),A  ; 3
+ld (mmu_i2cA0),A  ; 3
+
+rl A              ; 1
+ld (mmu_i2cA0),A  ; 3
+ld (mmu_i2cA1),A  ; 3
+ld (mmu_i2cA0),A  ; 3
+
+; I2C: Accept slave acknowledgement (SAK).
+
+ld (mmu_i2cZ0),A  ; 3
+ld (mmu_i2cZ1),A  ; 3
+ld (mmu_i2cZ0),A  ; 3
+
+; I2C: Stop code (SP).
+
+ld (mmu_i2c00),A  ; 3
+ld (mmu_i2c01),A  ; 3
+ld (mmu_i2cZ1),A  ; 3
+     
+ret
+
+
+; ------------------------------------------------------------
 ; I2C Sixteen-Bit Read. Read two consecutive bytes from the sensor
 ; address map. The address of the first byte should be passed into
 ; the routine in the accumulator. The first byte read will be returned
 ; in B, the second in A.
+
 i2c_rd16:
 
 ; Store the sub-address address in B.
@@ -344,225 +722,4 @@ ld (mmu_i2cZ1),A  ; 3
 ret
 
 ; ------------------------------------------------------------
-; The interrupt routine. Reads the sensor and transmits the
-; measurement with the sample transmitter.
-interrupt:
 
-; Save A on the stack, set bit zero to one, use to enable
-; the transmit clock and then boost the CPU to TCK. Push
-; F, B, and C onto stack.
-push A       
-ld A,0x01          
-ld (mmu_etc),A    
-ld (mmu_bcc),A
-push F  
-push B  
-push C
-
-; Set the zero bit of the diagnostic flag register. We can
-; direct this bit to a test pin to see if the interrupt is
-; happening. We read the register and OR with 0x01 to set
-; the zero bit.
-ld A,(mmu_dfr)     
-or A,0x01          
-ld (mmu_dfr),A  
-
-; Sixteen-bit read from sensor. The first byte will be in B
-; and the second in A.
-ld A,ps_WHO_AM_I
-call i2c_rd16
-
-; Transfer the two bytes into the transmit data registers.
-ld (mmu_xlb),A
-push B
-pop A
-ld (mmu_xhb),A
-
-; Write the device identifier to the transmit channel number register.
-ld A,device_id   
-ld (mmu_xcn),A 
-
-; Initiate transmission with any write to the transmission control
-; register.
-ld (mmu_xcr),A 
-
-; Wait for transmission to complete. We must keep the transmit clock going
-; so the state machine will finish its transmission.
-ld A,tx_delay    
-dly A            
-
-; Reset all interrupts.
-ld A,0xFF   
-ld (mmu_irst),A   
-
-; Clear bit zero of the diagnostic flag register to indicate the end
-; of the interrupt.
-ld A,(mmu_dfr)  
-and A,0xFE   
-ld (mmu_dfr),A 
-
-; Restore C, B, and F.
-pop C
-pop B
-pop F 
-
-; Move the CPU out of boost, stop the transmit clock, pop A, and 
-; return from interrupt.
-ld A,0x00        
-ld (mmu_bcc),A    
-ld (mmu_etc),A 
-pop A
-rti  
-
-; ------------------------------------------------------------
-; Calibrate the transmit clock frequency. Will leave the
-; transmit clock disabled and cpu boost turned off. The 
-; transmit clock should run at 5 MHz. We are going to determine
-; which value of transmit clock divisor, when applied to the
-; ring oscillator frequency, gives us a frequency close to
-; 5 MHz. We will write that value to the transmit clock divisor
-; register, and so calibrate the ring oscillator.
-calibrate_tck:
-
-; Push the registers we are going to use.
-push F
-push A           
-push B   
-
-; Disable the CPU boost clock, to enter non-boost, where we are
-; running off 32.768 kHz.
-ld A,0x00    
-ld (mmu_bcc),A   
-
-; Turn off the transmit clock (TCK) by writing a zero to the
-; enable transmit clock location in our control space. 
-ld (mmu_etc),A
-
-; Pick an initial value for the divisor that we know will be 
-; too high, so the transmit clock frequency will certainly
-; be too low. Store this value in both A and B registers. There
-; is no instruction to move A directly into B. Instead, we push
-; A onto the stack, then pop it off into B.
-ld A,max_tcd 
-push A  
-pop B
-
-; We enter a loop in which we decrement the divisor stored in
-; B, then try it out and see if the resulting frequency is 
-; above the minimum frequency we are prepared to accept. We
-; measure the frequency using the transmit clock frequency
-; register, which counts the number of TCK cycles in one
-; half of an RCK cycle immediately after we enable the 
-; transmit clock. When we have this number of cycles, we 
-; subtract a minimum, and if the result is negative, we know
-; we have to decrease the divisor further. If the result is
-; positive, we are done.
-cal_tck_1:
-dec B           
-push B           
-pop A            
-ld (mmu_tcd),A   
-ld A,0x01        
-ld (mmu_etc),A   
-ld A,(mmu_tcf)  
-sub A,min_tcf   
-ld A,0x00       
-ld (mmu_etc),A   
-jp np,cal_tck_1 
-
-; Pop and return.
-pop B            
-pop A          
-pop F
-ret              
-
-; ------------------------------------------------------------
-; Sensor initialization routine. Write to all registers we need to
-; configure to obtain pressure and temperature measurements of the
-; desired range and precision.
-sensor_init:
-
-push F
-push A  
-push B
-push C
-
-;ld A,0x31
-;push A
-;pop C
-;ld A,ps_CTRL1
-;call i2c_wr8
-
-pop C
-pop B
-pop A            
-pop F 
-ret   
-
-
-; ------------------------------------------------------------
-; Initialize the device. We will be setting up the stack, calibrating
-; the ring oscillator, and configuring the sensor.
-initialize:
-
-; Initialize the stack pointer.
-ld HL,mmu_sba
-ld SP,HL
-
-; The boot delay allows the power supplies to settle before we calibrate
-; the ring oscillator.
-ld A,boot_delay  ; We want boot_delay
-push A           ; multiplied by 256 cycles of RCK
-pop B            ; before proceeding.
-pwr_up_lp:
-ld A,0xFF        ; Load A with 255
-dly A            ; and delay,
-dec B            ; then decrement B.
-jp nz,pwr_up_lp
-
-; Calibrate the transmit clock.
-call calibrate_tck
-
-; Set the low radio frequency for sample transmission
-ld A,tx_frequency
-ld (mmu_xfc),A
-
-; Initialize the sensor.
-call sensor_init
-
-; Set interrupt timer interval and enable the timer interrupt to implement
-; the sample period. The value we want to load into the interrup timer period 
-; register is the sample period minus one, because the interrupt timer counts
-; the value down to zero. So we load A with sample_period, then decrement. If
-; sample_period is zero (0x00), the value we write is 255 (0xFF).
-ld A,0xFF           
-ld (mmu_irst),A  
-ld A,sample_period 
-dec A 
-ld (mmu_itp),A 
-ld A,0x01 
-ld (mmu_imsk),A 
-
-; Done with initialization, jump to main loop.
-jp main
-
-
-; ------------------------------------------------------------
-
-; The main program loop. The interrupts will be running 
-; in the background, and they do all the work. The main
-; routine generates a pulse on bit one of the diagnostic
-; flag register, and it implements a delay so these 
-; pulses are rare.
-main:
-
-ld A,(mmu_dfr) 
-or A,0x02
-ld (mmu_dfr),A
-and A,0xFD
-ld (mmu_dfr),A
-ld A,255  
-dly A      
-jp main
-
-; ------------------------------------------------------------
